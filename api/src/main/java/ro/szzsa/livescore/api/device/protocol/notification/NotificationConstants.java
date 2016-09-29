6e6d98e796da91ef2f035948de25fc8a2ee15a89
Attila Szasz <attila.szasz@iquestgroup.com>
2016-09-29 11:31:11 +0300

    Add stats notification topic

---
+++ api/src/main/java/ro/szzsa/livescore/api/device/protocol/notification/NotificationConstants.java
@@ -0,0 +1,13 @@
+package ro.szzsa.livescore.api.device.protocol.notification;
+
+/**
+ *
+ */
+public final class NotificationConstants {
+
+  public static final String STATS_TOPIC_NAME = "stats";
+
+  private NotificationConstants() {
+    throw new UnsupportedOperationException();
+  }
+}
