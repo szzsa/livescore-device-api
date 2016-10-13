72348defa5eee4264fc4e072f85eec323032fc71
Attila Szasz <attila.szasz@iquestgroup.com>
2016-10-13 13:40:11 +0300

    Add missing fields to notification model

--- api/src/main/java/ro/szzsa/livescore/api/device/protocol/notification/Notification.java
+++ api/src/main/java/ro/szzsa/livescore/api/device/protocol/notification/Notification.java
@@ -6,10 +6,16 @@ package ro.szzsa.livescore.api.device.protocol.notification;
 public class Notification {
 
   public static final String STATS_TOPIC_NAME = "stats";
+  public static final String MESSAGE_KEY = "message";
+  private static final int DEFAULT_TIME_TO_LIVE = 300;
 
   private String to;
 
   private Data data;
+
+  private String collapse_key;
+
+  private int time_to_live = DEFAULT_TIME_TO_LIVE;
 
   public String getTo() {
     return to;
@@ -25,5 +31,21 @@ public class Notification {
 
   public void setData(Data data) {
     this.data = data;
+  }
+
+  public String getCollapse_key() {
+    return collapse_key;
+  }
+
+  public void setCollapse_key(String collapse_key) {
+    this.collapse_key = collapse_key;
+  }
+
+  public int getTime_to_live() {
+    return time_to_live;
+  }
+
+  public void setTime_to_live(int time_to_live) {
+    this.time_to_live = time_to_live;
   }
 }
